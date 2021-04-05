d08797b8707e65dc4a22fd5d54901319f1328652
Shubham Sahu <shubham1208@users.noreply.github.com>
2021-04-05 12:18:32 +0530

    final mechanism

--- sudoku.py
+++ sudoku.py
@@ -10,12 +10,44 @@ board = [
     [0,0,0,0,8,0,0,7,9]
 ]
 
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+
+    return False
+
 def valid(board, num, pos):
     
     for i in range(len(board[0])):
         if board[pos[0]][i] == num and pos[1] != i:
             return False
         
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3,  box_x * 3 +3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 def disp_board(board):
     for i in range(len(board)):
@@ -37,6 +69,9 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return(i, j) #y, x
+    return None
 
-
+disp_board(board)
+solve(board)
+print('')
 disp_board(board)
